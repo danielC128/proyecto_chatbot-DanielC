8e25c38e8b607368f339726d976baf1f2191c5f3
root <root@DESKTOP-7NF9TD2.>
2025-02-28 14:24:00 -0500

    subiendo archivos a nueva rama

--- run.sh
+++ run.sh
@@ -6,3 +6,4 @@ gunicorn -c gunicorn_config.py app:app
     # Intencion "agendar cita" -> chatgpt nos debe decir el dia y hora en formato AAAA-MM-DD HH:MM, agendamos esta cita en una base de datos podria ser donde esten las citas no confirmadas y cada cierto tiempo limpiamos estas citas no confirmadas como por cada 2 horas.
     # Intencion "generar link de pago" -> generamos el link de pago con los datos del cliente y mandamos a chat esa informacion para que responda adecuadamente
     # Proceso interno -> "confirmar pago" -> nos llega la confirmación del pago, informamos al cliente y lo añadimos a la conversacion actual en la parte de respuesta del chatbot y agendamos ahora si la cita.
+
--- twilio_prueba.py
+++ twilio_prueba.py
@@ -1,13 +1,13 @@
-from components.twilio_component import TwilioManager
-from api_keys.api_keys import promesa_pago_interesados, promesa_pago
+#from components.twilio_component import TwilioManager
+#from api_keys.api_keys import promesa_pago_interesados, promesa_pago
 
-twilio_manager = TwilioManager()
-template_content_sid = promesa_pago_interesados
-parameters = '{"1": "John"}'  # JSON string con parámetros
+#twilio_manager = TwilioManager()
+#template_content_sid = promesa_pago_interesados
+#parameters = '{"1": "John"}'  # JSON string con parámetros
 
-sid = twilio_manager.send_template_message(
-    to_number='+51932709296',
-    template_content_sid=template_content_sid,
-    parameters=parameters
-)
+#sid = twilio_manager.send_template_message(
+#    to_number='+51932709296',
+#    template_content_sid=template_content_sid,
+#    parameters=parameters
+#)
 
